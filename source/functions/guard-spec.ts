a7452c2e73be74e445b2874b4a6201d4af8f5c5a
Nicholas Jamieson <nicholas@cartant.com>
2018-11-03 15:14:03 +1000

    chore(functions): Add missing tests.

--- source/functions/guard-spec.ts
+++ source/functions/guard-spec.ts
@@ -5,10 +5,38 @@
 /*tslint:disable:no-unused-expression*/
 
 import { expect } from "chai";
-import { usingEmpty, usingFsa, usingPayload, usingProps } from "./foobar-spec";
+import { usingCreator, usingEmpty, usingFsa, usingPayload, usingProps } from "./foobar-spec";
 import { guard } from "./guard";
 
 describe("classes/guard", () => {
+
+    describe("creator", () => {
+
+        const { bar, baz, foo } = usingCreator;
+
+        it("should return true for matching actions", () => {
+            const fooAction = foo(42);
+            expect(guard(foo)(fooAction)).to.be.true;
+            if (guard(foo)(fooAction)) {
+                expect(fooAction.foo).to.equal(42);
+            }
+        });
+
+        it("should return false for non-matching actions", () => {
+            const barAction = bar(54);
+            expect(guard(foo)(barAction)).to.be.false;
+        });
+
+        it("should return true for matching unions", () => {
+            const fooAction = foo(42);
+            expect(guard({ foo, bar })(fooAction)).to.be.true;
+        });
+
+        it("should return false for non-matching unions", () => {
+            const bazAction = baz(42);
+            expect(guard({ foo, bar })(bazAction)).to.be.false;
+        });
+    });
 
     describe("empty", () => {
 
@@ -43,6 +71,34 @@ describe("classes/guard", () => {
             const fooAction = foo({ foo: 42 });
             expect(guard(foo)(fooAction)).to.be.true;
             if (guard(foo)(fooAction) && !fooAction.error) {
+                expect(fooAction.payload.foo).to.equal(42);
+            }
+        });
+
+        it("should return false for non-matching actions", () => {
+            const barAction = bar({ bar: 54 });
+            expect(guard(foo)(barAction)).to.be.false;
+        });
+
+        it("should return true for matching unions", () => {
+            const fooAction = foo({ foo: 42 });
+            expect(guard({ foo, bar })(fooAction)).to.be.true;
+        });
+
+        it("should return false for non-matching unions", () => {
+            const bazAction = baz({ baz: 42 });
+            expect(guard({ foo, bar })(bazAction)).to.be.false;
+        });
+    });
+
+    describe("payload", () => {
+
+        const { bar, baz, foo } = usingPayload;
+
+        it("should return true for matching actions", () => {
+            const fooAction = foo({ foo: 42 });
+            expect(guard(foo)(fooAction)).to.be.true;
+            if (guard(foo)(fooAction)) {
                 expect(fooAction.payload.foo).to.equal(42);
             }
         });
--- source/functions/isType-spec.ts
+++ source/functions/isType-spec.ts
@@ -5,10 +5,38 @@
 /*tslint:disable:no-unused-expression*/
 
 import { expect } from "chai";
-import { usingEmpty, usingFsa, usingPayload, usingProps } from "./foobar-spec";
+import { usingCreator, usingEmpty, usingFsa, usingPayload, usingProps } from "./foobar-spec";
 import { isType } from "./isType";
 
 describe("classes/isType", () => {
+
+    describe("props", () => {
+
+        const { bar, baz, foo } = usingCreator;
+
+        it("should return true for matching actions", () => {
+            const fooAction = foo(42);
+            expect(isType(fooAction, foo)).to.be.true;
+            if (isType(fooAction, foo)) {
+                expect(fooAction.foo).to.equal(42);
+            }
+        });
+
+        it("should return false for non-matching actions", () => {
+            const barAction = bar(54);
+            expect(isType(barAction, foo)).to.be.false;
+        });
+
+        it("should return true for matching unions", () => {
+            const fooAction = foo(42);
+            expect(isType(fooAction, { foo, bar })).to.be.true;
+        });
+
+        it("should return false for non-matching unions", () => {
+            const bazAction = baz(42);
+            expect(isType(bazAction, { foo, bar })).to.be.false;
+        });
+    });
 
     describe("empty", () => {
 
