9e317a48a557b2bdbd0fa47d43cc5b588873b30e
Nicholas Jamieson <nicholas@cartant.com>
2019-04-27 11:44:27 +1000

    refactor(act): Use materialize and type aliases.

--- packages/ts-action-operators/source/act.ts
+++ packages/ts-action-operators/source/act.ts
@@ -2,17 +2,25 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/ts-action
  */
+/*tslint:disable:rxjs-no-unsafe-scope*/
 
 import { Action } from "ts-action";
 import {
   defer,
   merge,
+  Notification,
   Observable,
   OperatorFunction,
-  Subject,
-  Subscription
+  Subject
 } from "rxjs";
-import { concatMap } from "rxjs/operators";
+import {
+  concatMap,
+  dematerialize,
+  filter,
+  finalize,
+  map,
+  materialize
+} from "rxjs/operators";
 
 enum UnspecifiedBrand {}
 type UnspecifiedAction = {
@@ -23,7 +31,7 @@ type SpecifiedAction<A extends Action> = Exclude<A, UnspecifiedAction>;
 
 export function act<
   EffectAction extends Action,
-  ProjectAction extends Action = UnspecifiedAction,
+  ProjectedAction extends Action = UnspecifiedAction,
   ErrorAction extends Action = UnspecifiedAction,
   CompleteAction extends Action = UnspecifiedAction,
   UnsubscribeAction extends Action = UnspecifiedAction
@@ -39,72 +47,73 @@ export function act<
   operator?: <A, R>(
     project: (action: A, index: number) => Observable<R>
   ) => OperatorFunction<A, R>;
-  project: (action: EffectAction, index: number) => Observable<ProjectAction>;
+  project: (action: EffectAction, index: number) => Observable<ProjectedAction>;
   unsubscribe?: (count: number, action: EffectAction) => UnsubscribeAction;
 }): (
   source: Observable<EffectAction>
 ) => Observable<
   SpecifiedAction<
-    ProjectAction | ErrorAction | CompleteAction | UnsubscribeAction
+    ProjectedAction | ErrorAction | CompleteAction | UnsubscribeAction
   >
 > {
+  type CombinedAction =
+    | ProjectedAction
+    | ErrorAction
+    | CompleteAction
+    | UnsubscribeAction;
+  type ReturnedAction = SpecifiedAction<
+    ProjectedAction | ErrorAction | CompleteAction | UnsubscribeAction
+  >;
   return source =>
     defer(
-      (): Observable<Action> => {
+      (): Observable<CombinedAction> => {
         const subject = new Subject<UnsubscribeAction>();
         return merge(
           source.pipe(
-            operator(
-              (action, index) =>
-                new Observable(subscriber => {
-                  let completed = false;
-                  let errored = false;
-                  let projected = 0;
-                  const subscription = new Subscription();
-                  subscription.add(() => {
+            operator((action, index) =>
+              defer(() => {
+                let completed = false;
+                let errored = false;
+                let projected = 0;
+                return project(action, index).pipe(
+                  materialize(),
+                  map(
+                    (note): Notification<CombinedAction> | undefined => {
+                      switch (note.kind) {
+                        case "E":
+                          errored = true;
+                          return new Notification(
+                            "N",
+                            error(note.error, action)
+                          );
+                        case "C":
+                          completed = true;
+                          return complete
+                            ? new Notification("N", complete(projected, action))
+                            : undefined;
+                        default:
+                          ++projected;
+                          return note;
+                      }
+                    }
+                  ),
+                  filter(isDefined),
+                  dematerialize(),
+                  finalize(() => {
                     if (!completed && !errored && !projected && unsubscribe) {
                       subject.next(unsubscribe(projected, action));
                     }
-                  });
-                  subscription.add(
-                    project(action, index).subscribe({
-                      complete() {
-                        completed = true;
-                        if (complete) {
-                          try {
-                            subscriber.next(complete(projected, action));
-                          } catch (callbackError) {
-                            subscriber.error(callbackError);
-                            return;
-                          }
-                        }
-                        subscriber.complete();
-                      },
-                      error(projectError: any) {
-                        errored = true;
-                        try {
-                          subscriber.next(error(projectError, action));
-                        } catch (callbackError) {
-                          subscriber.error(callbackError);
-                        }
-                        subscriber.complete();
-                      },
-                      next(projectAction: ProjectAction) {
-                        ++projected;
-                        subscriber.next(projectAction);
-                      }
-                    })
-                  );
-                  return subscription;
-                })
+                  })
+                );
+              })
             )
           ),
           subject
         );
       }
-    ) as Observable<
-      SpecifiedAction<
-        ProjectAction | ErrorAction | CompleteAction | UnsubscribeAction
-      >
-    >;
+    ) as Observable<ReturnedAction>;
+}
+
+function isDefined<T>(value: T | undefined): value is T {
+  return value !== undefined;
 }
