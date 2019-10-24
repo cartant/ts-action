54bd231582d3202bd5e873ce5f76b0a5615c433f
Nicholas Jamieson <nicholas@cartant.com>
2019-10-24 18:42:17 +1000

    chore: Add experiment placeholder.

---
+++ packages/ts-action/source/reducer-experiment-spec.ts
@@ -0,0 +1,14 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/ts-action
+ */
+/*tslint:disable:no-unused-expression*/
+
+import { timeout } from "./snippet-spec";
+
+describe("reducer-experiment", function(): void {
+  /*tslint:disable-next-line:no-invalid-this*/
+  this.timeout(timeout);
+
+  it.skip("should experiment", () => {});
+});
